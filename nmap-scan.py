a66013296a0269dd73059241ce9606b7aa5576bb
Nuudlikauss <allan.albert25@gmail.com>
2017-03-27 10:54:57 +0300

    Update nmap-scan.py

--- nmap-scan.py
+++ nmap-scan.py
@@ -1,7 +1,4 @@
-Python 3.5.2 (default, Nov 17 2016, 17:05:23) 
-[GCC 5.4.0 20160609] on linux
-Type "copyright", "credits" or "license()" for more information.
->>> import csv
+import csv
 import nmap
 import sys
 
